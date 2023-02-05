3801e7179aeaf6d4d0474916e66b5fc337dd465c
Infernet89 <infernet2002@libero.it>
2023-02-05 09:40:07 +0100

    gitArt  WAS  NOT  35

--- create.py
+++ create.py
@@ -72,7 +72,7 @@ def createCommit(word):
 	for day in res:
 		command='git commit --date=\''
 		command+=(datetime.date(year, 1, 1) + datetime.timedelta(days=day)).strftime('%Y-%m-%d')
-		command+='\' -am \'gitArt day '+str(day)+'\' --allow-empty'
+		command+='\' -am \'gitArt '+word+' '+str(day)+'\' --allow-empty'
 		#print(command)
 		os.system(command)
 	return
